025fbd8ba47fa5f8183c8138fe8f8352891103c9
Josh Adams <josh.rubyist@gmail.com>
2017-09-22 14:01:00 -0500

    Seems to get robots.txt for a given domain now

--- lib/webcrawler.ex
+++ lib/webcrawler.ex
@@ -1,14 +1,25 @@
 defmodule Webcrawler do
   require Tesla
 
-  def get_robots_for(host) do
-    response = Tesla.get(client, "http://google.com/robots.txt")
+  def get_robots_for(uri_string) do
+    response = Tesla.get(client, robots_txt(uri_string))
     response.body
   end
 
-  def client do
+  defp client do
     Tesla.build_client([
       {Tesla.Middleware.FollowRedirects, [max_redirects: 3]}
     ])
+  end
+
+  defp robots_txt(uri_string) do
+    result = URI.parse(uri_string)
+    scheme = result.scheme || "http"
+    case result.host do
+      nil ->
+        "#{scheme}://#{result.path}/robots.txt"
+      host ->
+        "#{scheme}://#{host}/robots.txt"
+    end
   end
 end
--- test/webcrawler_test.exs
+++ test/webcrawler_test.exs
@@ -4,6 +4,9 @@ defmodule WebcrawlerTest do
 
   test "downloads the robots file" do
     google_robots = File.read!("./test/fixtures/robots/google.com/robots.txt")
+    tesla_robots = File.read!("./test/fixtures/robots/tesla.com/robots.txt")
     assert Webcrawler.get_robots_for("google.com") == google_robots
+    assert Webcrawler.get_robots_for("http://google.com/foo") == google_robots
+    assert Webcrawler.get_robots_for("tesla.com") == tesla_robots
   end
 end
